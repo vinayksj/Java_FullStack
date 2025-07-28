8e6d8837450f2c5fb8f23e0e08280bb926bad1dd
vinayksj <115333707+vinayksj@users.noreply.github.com>
2025-07-28 10:22:25 +0530

    commit

---
+++ test/src/Day4/w31.java
@@ -0,0 +1,17 @@
+package Day4;
+
+import java.util.Scanner;
+
+public class w31 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number ");
+       int a= sc.nextInt();	
+       if (a>0)
+    	   System.out.println("number is positive");
+       else
+    	   System.out.println("number is negative");
+    	   }
+
+}
